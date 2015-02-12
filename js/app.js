8f3d5c7a896f94e72f6b2d6130958720282767f8
zdenekmacura <zdenek.macura@email.cz>
2015-02-12 04:31:20 +0100

    some changes

--- js/app.js
+++ js/app.js
@@ -7,22 +7,11 @@ function init() {
 function doContactPicker() {
 	navigator.contacts.pickContact(function(contact){
 		console.log('The following contact has been selected:' + JSON.stringify(contact));
-		//Build a simple string to display the Contact - would be better in Handlebars
-		var s = "";
-		s += "<h2>"+getName(contact)+"</h2>";
-
-		if(contact.emails && contact.emails.length) {
-			s+= "Email: "+contact.emails[0].value+"<br/>";
-		}
-
+		var s = "Vybraná vytýkaná osoba:";
+		s += getName(contact);
 		if(contact.phoneNumbers && contact.phoneNumbers.length) {
-			s+= "Phone: "+contact.phoneNumbers[0].value+"<br/>";
+			s+= ", Phone: "+contact.phoneNumbers[0].value+"<br/>";
 		}
-
-		if(contact.photos && contact.photos.length) {
-			s+= "<p><img src='"+contact.photos[0].value+"'></p>";
-		}
-
 		document.querySelector("#selectedContact").innerHTML=s;
 	},function(err){
 		console.log('Error: ' + err);
